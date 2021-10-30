3c523c90d312017b495ba60a6dbcd72401083043
Gustavo Schultz <83468422+Gultzz@users.noreply.github.com>
2021-10-29 23:44:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let buttonNo = document.querySelector('#buttonNo');
+buttonNo.onmouseover = trocarLugar;
+buttonNo.onclick = parabens;
+
+let buttonYes = document.querySelector('#buttonYes');
+buttonYes.onclick = alegrarSe;
+
+function trocarLugar(){
+    buttonNo.style.left = Math.floor(Math.random() * 75) + '%';
+    buttonNo.style.top = Math.floor(Math.random() * 90) + '%';
+    console.log(buttonNo.style.left);
+    console.log(buttonNo.style.top);
+}
+
+function alegrarSe(){
+    window.location.href = 'https://www.kayak.com.br/news/lua-de-mel-no-brasil/';
+}
+
+function parabens(){
+    alert("Parabens, você realmente não gosta dele(a)");
+}
