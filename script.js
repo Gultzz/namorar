db0459099815a53d4d78851fe8140d208a7dd006
Gustavo Schultz <83468422+Gultzz@users.noreply.github.com>
2021-10-30 00:10:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let buttonNo = document.querySelector('#buttonNo');
+let tentativas = 0;
 buttonNo.onmouseover = trocarLugar;
 buttonNo.onclick = parabens;
 
@@ -6,6 +7,7 @@ let buttonYes = document.querySelector('#buttonYes');
 buttonYes.onclick = alegrarSe;
 
 function trocarLugar(){
+    tentativas++;
     buttonNo.style.left = Math.floor(Math.random() * 75) + '%';
     buttonNo.style.top = Math.floor(Math.random() * 90) + '%';
     console.log(buttonNo.style.left);
@@ -17,5 +19,5 @@ function alegrarSe(){
 }
 
 function parabens(){
-    alert("Parabens, você realmente não gosta dele(a)");
+    alert("Parabens, depois de "+ tentativas+" tentativas descobrimos que você realmente não gosta dele(a)");
 }
